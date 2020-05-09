1e560d4678e746d2a8ac068cd120fbcb9cb7f07b
lhiakrul <711hikaru@gmail.com>
2020-05-09 14:13:27 +0800

    add: import 遊戲圖片

--- src/js/base/Resource.js
+++
@@ -1,8 +0,0 @@
-export const Resources = [
-	['background', 'image/background.png'],
-	['land', 'image/land.png'],
-	['pencilUp', 'image/pie_up.png'],
-	['pencilDown', 'image/pie_down.png'],
-	['birds', 'image/birds.png'],
-	['startButton', 'image/start_button.png']
-];

8d9bffe297169451c99b5eb8d2b0ac2ed0948008
lhiakrul <711hikaru@gmail.com>
2020-05-09 14:17:43 +0800

    add: url-loader package

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,24 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname,'bundle')
+    },
+    resolve: {
+        alias: {
+            image: path.resolve(__dirname, 'src/image'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|png)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: '/images'
+                    }
+                }
+            }
+        ]
     }
 }

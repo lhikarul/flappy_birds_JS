b6fb507a11a44c3fc5a728c99835dd0c0bff90e8
lhiakrul <711hikaru@gmail.com>
2020-05-09 14:16:42 +0800

    add: 入口文件加載圖片

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,15 @@
+import {ResourceLoader} from './js/base/ResourceLoader';
 export class Main {
     constructor() {
-        console.log('hell222222o')
+       this.canvas = document.getElementById('game_canvas');
+       this.ctx = this.canvas.getContext('2d');
+    
+       const loader = ResourceLoader.create();
+
+       loader.onLoaded(map => this.onResourceFirstLoaded(map))
+
+    }
+    onResourceFirstLoaded(map) {
+        console.log('map ',map);
     }
 }

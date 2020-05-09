8ae97fb9e275e5897fc513bdc9052b907fc1ab84
lhiakrul <711hikaru@gmail.com>
2020-05-09 14:15:49 +0800

    add: 圖片加載

--- src/js/base/ResourceLoader.js
+++ src/js/base/ResourceLoader.js
@@ -1,4 +1,29 @@
 // 資源文件加載，確保 canvas 圖片資源加載完成之後，才進行渲染
+import {Resources} from './Resources';
 export class ResourceLoader {
+    constructor() {
+        this.map = new Map(Resources);
+        
+        for (let [key,value] of this.map) {
+            const image = new Image();
+            image.src = value;
+            this.map.set(key,image);
+        }
+    }
 
+    onLoaded(callback) {
+        let loadedCount = 0;
+        for (let value of this.map.values()) {
+            value.onload = () => {
+                loadedCount += 1;
+                if (loadedCount >= this.map.size) {
+                    callback(this.map);
+                }
+            }
+        }
+    }
+
+    static create() {
+        return new ResourceLoader();
+    }
 }
